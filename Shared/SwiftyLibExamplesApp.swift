644d6c0279ae4382dfad62f4532683c7db7abeab
Lima-Amanda <chioma.mmegwa@wizeline.com>
2022-03-14 18:17:32 +0100

    Initial project setup

---
+++ Shared/SwiftyLibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLibExamplesApp.swift
+//  Shared
+//
+//  Created by Chioma Amanda Mmegwa on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyLibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
