0d659b1160b80d2c1c87328bc9a2fcfaecdccccc
Lima-Amanda <chioma.mmegwa@wizeline.com>
2022-03-14 18:53:29 +0100

    Added test cases

---
+++ SwiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Chioma Amanda Mmegwa on 14/03/2022.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
